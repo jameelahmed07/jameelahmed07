f5f4948692f5b7de07863a1c50c83ad8c5220fd3
jameelahmed07 <87141511+jameelahmed07@users.noreply.github.com>
2021-07-08 21:55:58 +0530

    Add files via upload

---
+++ basic2.java
@@ -0,0 +1,18 @@
+package almmatter;
+
+import java.util.Scanner;
+
+public class basic2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter value for i..:");
+        int i=scan.nextInt();
+        System.out.println("Enter value for j...:");
+        int j=scan.nextInt();
+        int mul=i*j;
+        System.out.println("The Multiplication of i and j is..:"+mul);
+	}
+
+}
---
+++ printAnumber.java
@@ -0,0 +1,15 @@
+package almmatter;
+
+import java.util.Scanner;
+
+public class printAnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner reader= new Scanner(System.in);
+        System.out.println("enter a number");
+        int number=reader.nextInt();
+        System.out.println("you entered: "+number); 
+	}
+
+}
